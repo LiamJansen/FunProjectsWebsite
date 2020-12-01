90c21c8a8ad9591860de2c910b95579ca4a8321c
LiamJansen <62886309+LiamJansen@users.noreply.github.com>
2020-12-02 00:00:17 +0100

    Adding some descriptions,, moving TODO to issues, testing github desktop
    Testing committing with github desktop

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # FunProjectsWebsite
-Small website to explore fun, little ideas 
-
-TO DO:
-- Look into automatically updating pictures/quotes at a less-active time in the future
-- Images are too restricted by the default bootstrap config and are often too small
-- Being able to navigate by id number, GET request?
-- Randomize button for image/quote, randomize on page load
+This is just a small website to explore some ideas and to serve as a hub for various smaller items I like and want available when I might not be at my computer.
--- index.php
+++ index.php
@@ -16,8 +16,18 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                <p class="text-center"><span style="font-family: 'Beckett-Kanzlei', 'Times New Roman', serif; font-size: 50px"
-                     class="text-center">Welcome</span><br> to my site of fun, little projects to play with!</p>
+                    <p class="text-center"><span class="text-center beckett_kanzlei_font">Welcome</span></br></p>
+                    <p>
+                        My name is Liam and I'm currently searching for a software developer position.
+                        <br><br>
+                        I've created this little website for some of my personal stuff to have fun with.
+                        <br>
+                        There are pictures of beautiful landscapes, amazing original art or fanart of shows and films and epic structures to name a few examples.
+                        <br>
+                        There is also the quotes section where I've added quotes that inspire me or I think are just plain cool. These are from various media such as books or films, or just randomly found on the internet.
+                        <br>
+                        In the future I might add stories or drawings that I'm willing to make public.
+                    </p>
                 </div>
             </div>
         </div>
